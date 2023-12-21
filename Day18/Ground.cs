3e1a267999c773a4bcb782c26ee8e6b2eebf24ef
Hilde Hamer <hildehamer@hotmail.com>
2023-12-21 18:30:53 +0100

    Day18 part1  progress

---
+++ Day18/Ground.cs
@@ -0,0 +1,95 @@
+﻿using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Day18
+{
+    //To low 13237
+    //To low 39097
+    //
+
+    public class Ground
+    {
+        private List<Tuple<int, int>> ground = new List<Tuple<int, int>>();
+
+        public double Dig(List<string> list)
+        {
+            var startIndex = new Tuple<int, int>(0, 0);
+            ground.Add(startIndex);
+
+            var currentIndex = startIndex;
+            for(var row = 0; row < list.Count; row++)
+            {    
+                var values = list[row].Split(" ");
+                var direction = values[0];
+                var length = values[1];
+                var color = values[2];
+
+                currentIndex = AddNewDirections(currentIndex, int.Parse(length), direction);
+               
+            }
+
+            return PicksTheorem(ground);
+        }
+
+        public double CalFromMap(List<string> map)
+        {
+            for(var row = 0; row < map.Count; row++)
+            {
+                var items = map[row].ToCharArray().Select(c => c.ToString()).ToList();
+                for(var col = 0; col < items.Count; col++)
+                {
+                    if (items[col].Equals("#"))
+                    {
+                        ground.Add(new Tuple<int, int>(row, col));
+                    }     
+                }       
+            }
+
+            return PicksTheorem(ground);
+        }
+
+        public double PicksTheorem(List<Tuple<int, int>> corners)
+        {
+            var totals = new List<int>();
+            var test = corners.Aggregate((current, next) => current.Item2 * next.Item1 - current.Item1 * next.Item2).ToList();
+
+            for (var row = 0; row < corners.Count()-1; row++)
+            {
+                var current = corners[row];
+                var next = corners[row + 1];
+
+                var left = current.Item2 * next.Item1;
+                var rigth = current.Item1 * next.Item2;
+
+                totals.Add(left - rigth);
+            }
+            
+            double area = totals.Sum() /2;
+
+            return area + (corners.Count/ 2) +1;
+        }
+
+        private Tuple<int, int> AddNewDirections(Tuple<int,int> index, int length, string direction)
+        {
+            var lastIndex = new Tuple<int, int>(index.Item1, index.Item2);
+            switch (direction)
+            {
+                case "R":
+                    lastIndex = new Tuple<int, int>(index.Item1, index.Item2 + length);
+                    break;
+                case "L":
+                    lastIndex = new Tuple<int, int>(index.Item1, index.Item2 - length);
+                    break;
+                case "U":
+                    lastIndex = new Tuple<int, int>(index.Item1 - length, index.Item2);
+                    break;
+                case "D":
+                    lastIndex = new Tuple<int, int>(index.Item1 + length, index.Item2);
+                    break;
+            }
+            
+            ground.Add(lastIndex);
+            return lastIndex;
+        }
+    }
+}
---
+++ Day18/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day18;
+using System.Diagnostics;
+
+string path = "puzzle_input.txt";
+
+var currentLint = File.ReadLines(path).ToList();
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+var ground = new Ground();
+var result = ground.Dig(currentLint);
+sw.Stop();
+
+
+Console.WriteLine($"Answer Day 18 Answer 1: {result} time: {sw.ElapsedMilliseconds} ms");
+
+
+Stopwatch sw2 = new Stopwatch();
+sw2.Start();
+sw2.Stop();
+
+
+Console.WriteLine($"Answer Day 18 Answer 2:  time: {sw2.ElapsedMilliseconds} ms");
+

ba398753f319cbd88d25e3457b6506f4e7e1f78d
Hilde Hamer <hildehamer@hotmail.com>
2023-12-22 10:54:20 +0100

    Day 18 part 2 solved

--- Day18/Ground.cs
+++ Day18/Ground.cs
@@ -1,4 +1,5 @@
-﻿using System.Drawing;
+﻿using System.Collections.Generic;
+using System.Drawing;
 
 namespace Day18
 {
@@ -8,11 +9,11 @@ namespace Day18
 
     public class Ground
     {
-        private List<Tuple<int, int>> ground = new List<Tuple<int, int>>();
+        private List<Tuple<long, long>> ground = new List<Tuple<long, long>>();
 
         public double Dig(List<string> list)
         {
-            var startIndex = new Tuple<int, int>(0, 0);
+            var startIndex = new Tuple<long, long>(0, 0);
             ground.Add(startIndex);
 
             var steps = 0;
@@ -30,10 +31,36 @@ namespace Day18
 
            return PicksTheorem(ground, steps);
         }
-        
-        public double PicksTheorem(List<Tuple<int, int>> corners, int steps)
+
+        public double Dig2(List<string> list)
         {
-            var totals = new List<int>();
+            var startIndex = new Tuple<long, long>(0, 0);
+            ground.Add(startIndex);
+
+            var steps = 0L;
+            var currentIndex = startIndex;
+            for (var row = 0; row < list.Count; row++)
+            {
+                var values = list[row].Split(" ");
+                var color = values[2];
+
+                color = color.Replace(" ", "")
+                    .Replace("(", "")
+                    .Replace(")", "");
+                var hex = color.Substring(1, color.Length-2);
+                var length = Int32.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+                var direction = color.Replace(" ","").Substring(color.Length-1, 1);
+
+                currentIndex = AddNewDirections(currentIndex, length, direction);
+                steps += length;
+            }
+
+            return PicksTheorem(ground, steps);
+        }
+
+        public double PicksTheorem(List<Tuple<long, long>> corners, long steps)
+        {
+            var totals = new List<long>();
 
             for (var row = 0; row < corners.Count()-1; row++)
             {
@@ -51,22 +78,22 @@ namespace Day18
             return area + (steps/ 2) +1;
         }
 
-        private Tuple<int, int> AddNewDirections(Tuple<int,int> index, int length, string direction)
+        private Tuple<long, long> AddNewDirections(Tuple<long,long> index, int length, string direction)
         {
-            var lastIndex = new Tuple<int, int>(index.Item1, index.Item2);
+            var lastIndex = new Tuple<long, long>(index.Item1, index.Item2);
             switch (direction)
             {
-                case "R":
-                    lastIndex = new Tuple<int, int>(index.Item1, index.Item2 + length);
+                case "R" or "0":
+                    lastIndex = new Tuple<long, long>(index.Item1, index.Item2 + length);
                     break;
-                case "L":
-                    lastIndex = new Tuple<int, int>(index.Item1, index.Item2 - length);
+                case "L" or "2":
+                    lastIndex = new Tuple<long, long>(index.Item1, index.Item2 - length);
                     break;
-                case "U":
-                    lastIndex = new Tuple<int, int>(index.Item1 - length, index.Item2);
+                case "U" or "3":
+                    lastIndex = new Tuple<long, long>(index.Item1 - length, index.Item2);
                     break;
-                case "D":
-                    lastIndex = new Tuple<int, int>(index.Item1 + length, index.Item2);
+                case "D" or "1":
+                    lastIndex = new Tuple<long, long>(index.Item1 + length, index.Item2);
                     break;
             }
             
--- Day18/Program.cs
+++ Day18/Program.cs
@@ -17,8 +17,10 @@ Console.WriteLine($"Answer Day 18 Answer 1: {result} time: {sw.ElapsedMillisecon
 
 Stopwatch sw2 = new Stopwatch();
 sw2.Start();
+var ground2 = new Ground();
+var result2 = ground2.Dig2(currentLint);
 sw2.Stop();
 
 
-Console.WriteLine($"Answer Day 18 Answer 2:  time: {sw2.ElapsedMilliseconds} ms");
+Console.WriteLine($"Answer Day 18 Answer 2: {result2} time: {sw2.ElapsedMilliseconds} ms");
 
